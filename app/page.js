ef648bb8e50d92ae706cfacec9222027ac8f7959
Amrit <amritsinghal2211@gmail.com>
2024-06-03 18:29:07 +0530

    server side components

--- app/page.js
+++ app/page.js
@@ -1,11 +1,14 @@
-"use client"
+import fs from "fs/promises"
 import React from 'react'
-import { useState ,useEffect } from 'react'
+// import { useState ,useEffect } from 'react'
 const page = () => {
-  const [count , setCount] = useState(0)
+  let a = fs.readFile(".gitignore");
+  a.then(e=>{console.log(e.toString( ))});
+  // const [count , setCount] = useState(0)
   return (
-    <div>I am component {count}
-    <button onClick={()=> setCount(count + 1)}>Click me</button>
+    <div>I am component 
+      {/* {count} */}
+    {/* <button onClick={()=> setCount(count + 1)}>Click me</button> */}
     </div>
     
   )
