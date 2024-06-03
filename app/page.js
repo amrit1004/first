ff02b49ae8462a1ad7f5ab93a74f2bd3aceea6ed
Amrit <amritsinghal2211@gmail.com>
2024-06-03 18:28:39 +0530

    server side components

--- app/page.js
+++ app/page.js
@@ -1,8 +1,13 @@
+"use client"
 import React from 'react'
-
+import { useState ,useEffect } from 'react'
 const page = () => {
+  const [count , setCount] = useState(0)
   return (
-    <div>I am HomePage</div>
+    <div>I am component {count}
+    <button onClick={()=> setCount(count + 1)}>Click me</button>
+    </div>
+    
   )
 }
 
