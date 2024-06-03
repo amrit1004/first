fe1f79a7d5de148f0ce0a4d6b82fe1671aa0dbb0
Amrit <amritsinghal2211@gmail.com>
2024-06-03 18:50:07 +0530

    script

--- app/page.js
+++ app/page.js
@@ -1,16 +1,10 @@
-import fs from "fs/promises"
 import React from 'react'
-// import { useState ,useEffect } from 'react'
-const page = () => {
-  let a = fs.readFile(".gitignore");
-  a.then(e=>{console.log(e.toString( ))});
-  // const [count , setCount] = useState(0)
+
+function page() {
   return (
-    <div>I am component 
-      {/* {count} */}
-    {/* <button onClick={()=> setCount(count + 1)}>Click me</button> */}
+    <div>
+        
     </div>
-    
   )
 }
 
